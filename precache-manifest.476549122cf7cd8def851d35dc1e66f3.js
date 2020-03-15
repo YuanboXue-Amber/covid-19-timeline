04680189fb20cec76565d60de972e9d23e0cfd73
Yuanbo Xue <yuanboxue@outlook.com>
2020-03-15 22:45:16 +0100

    Updates

---
+++ precache-manifest.476549122cf7cd8def851d35dc1e66f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490f03cbffadc89f4bd2fd2c305ccd21",
+    "url": "/covid-19-timeline/index.html"
+  },
+  {
+    "revision": "b0ceb606ae4a8138de08",
+    "url": "/covid-19-timeline/static/css/main.44c3e4bd.chunk.css"
+  },
+  {
+    "revision": "f04d55fa8aee5c1055cd",
+    "url": "/covid-19-timeline/static/js/2.7b42f43b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid-19-timeline/static/js/2.7b42f43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ceb606ae4a8138de08",
+    "url": "/covid-19-timeline/static/js/main.676afb0e.chunk.js"
+  },
+  {
+    "revision": "be19fe569b9709010633",
+    "url": "/covid-19-timeline/static/js/runtime-main.db15ee48.js"
+  }
+]);
